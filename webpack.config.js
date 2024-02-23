6de5f23b25db3c7f81d9e09f5496158eac3256bb
Branden Basche <brandenbasche@gmail.com>
2024-02-23 17:31:28 -0500

    implemented webpack and frontend boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, './client/index.js'),
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+  //   mode: process.env.NODE_ENV,
+  mode: 'development', 
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    static: {
+      directory: path.resolve(__dirname),
+      publicPath: '/'
+    },
+    proxy: [
+      {
+        context: ['/api'],
+        target: 'http://localhost:3000'
+      }
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
