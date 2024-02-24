526740b7e425ae1234cefa91469117aaadc9d392
Charlie Schubach <charlie.schubach@gmail.com>
2024-02-24 11:57:44 -0500

    connecting database

--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const path = require('path');
-
+const dbController = require('./controllers/dbController');
 const app = express();
-
+const testRouter = require('./routes/testRouter');
 const PORT = 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/test', testRouter);
 
 app.get('/', (req, res) => {
   res.status(200).send('hello world!');
 });
 
 // catch-all route handler for any requests to an unknown route
-app.use((req, res) => res.status(404).send('This is not the page you\'re looking for...'));
+app.use((req, res) =>
+  res.status(404).send("This is not the page you're looking for...")
+);
 
 /**
  * express error handler
@@ -30,12 +34,11 @@ app.use((err, req, res, next) => {
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-
 /**
  * start server
  */
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}...`);
 });
-  
+
 module.exports = app;
