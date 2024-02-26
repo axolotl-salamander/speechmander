d90da09a3dd782635b51edbfe5e528de7dfee4d1
Charlie Schubach <charlie.schubach@gmail.com>
2024-02-26 12:59:20 -0500

    bcrypt implementation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -10,37 +10,68 @@ const db = require('../models/dbModels.js');
 
 const userController = {};
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 userController.createUser = (req, res, next) => {
   const { username, email, password } = req.body;
-  const query = `INSERT INTO users (username, email, password) VALUES ('${username}', '${email}', '${password}');`;
-  db.query(query)
-    .then((data) => {
-      console.log(data);
-      return next();
-    })
-    .catch((err) => next(err));
+
+  // Hash the password before storing it in the database
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) return next(err);
+
+    const query = `INSERT INTO users (username, email, password) VALUES ('${username}', '${email}', '${hash}');`;
+    db.query(query)
+      .then((data) => {
+        console.log(data);
+        res.locals.user = { username, email };
+        return next();
+      })
+      .catch((err) => next(err));
+  });
 };
 
 userController.verifyUser = (req, res, next) => {
   const { username, password } = req.body;
-  const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}';`;
+
+  // Get the user from the database
+  const query = `SELECT * FROM users WHERE username = '${username}';`;
   db.query(query)
     .then((data) => {
-      res.locals.user = data.rows[0];
-      return next();
+      const user = data.rows[0];
+
+      // Compare the provided password with the hashed password in the database
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (err) return next(err);
+
+        if (result) {
+          // Passwords match
+          res.locals.user = user;
+          return next();
+        } else {
+          // Passwords don't match
+          return next(new Error('Invalid password'));
+        }
+      });
     })
     .catch((err) => next(err));
 };
 
 userController.updateUser = (req, res, next) => {
   const { username, email, password } = req.body;
-  const query = `UPDATE users SET email = '${email}', password = '${password}' WHERE username = '${username}';`;
-  db.query(query)
-    .then((data) => {
-      console.log(data);
-      return next();
-    })
-    .catch((err) => next(err));
+
+  // Hash the password before updating it in the database
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) return next(err);
+
+    const query = `UPDATE users SET email = '${email}', password = '${hash}' WHERE username = '${username}';`;
+    db.query(query)
+      .then((data) => {
+        console.log(data);
+        return next();
+      })
+      .catch((err) => next(err));
+  });
 };
 
 userController.deleteUser = (req, res, next) => {
