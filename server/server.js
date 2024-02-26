a88daca4b880924578d2295a80ab04675a44e3b1
Branden Basche <brandenbasche@gmail.com>
2024-02-26 10:48:56 -0500

    merged with dev

--- server/server.js
+++ server/server.js
@@ -2,15 +2,7 @@ const express = require('express');
 
 const app = express();
 
-<<<<<<< HEAD
-const deepgram = createClient('d3b121ea821296238a901f7eddf6733cfe477c92');
-
-// const testAudio = path.join(__dirname, '../client/assets/audio-test.mp3');
-
-const testRouter = require('./routes/testRouter');
-=======
 // const testRouter = require('./routes/testRouter');
->>>>>>> 70b595d20c37d8700773b06626550fa79d037bc1
 const apiRouter = require('./routes/apiRouter');
 const PORT = 3000;
 
@@ -27,30 +19,6 @@ app.get('/', (req, res) => {
   res.status(200).send('hello world!');
 });
 
-<<<<<<< HEAD
-app.post('/transcribe', async (req, res) => {
-  try {
-    const audio = req.body.audio;
-    
-    const { result, error } = await deepgram.listen.prerecorded.transcribeFile(
-      fs.readFileSync(testAudio),
-      {
-        model: 'nova-2',
-        smart_format: true,
-        language: 'en-US',
-      }
-    );   
-    return res.status(200).send(result);
-  } catch (err) {
-    console.error('Error: ', err);
-    res.status(500).send('Error transcribing audio.');
-  }
-});
-// route to all request from client database
-app.use('/api', apiRouter);
-
-=======
->>>>>>> 70b595d20c37d8700773b06626550fa79d037bc1
 // catch-all route handler for any requests to an unknown route
 app.use((req, res) =>
   res.status(404).send("This is not the page you're looking for...")
